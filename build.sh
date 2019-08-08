dd6c73cfea95c7e6ef1123f2fa06c825c10cdcbf
Eyk Rehbein <eykrehbein@gmail.com>
2019-08-08 15:24:06 +0200

    Edit build script

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-go build -o bin/qmhelper src/qmhelper.go
-cp src/quickmove.sh bin/qmscript
---
+++ build_dev.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+go build -o bin/qcdhelper src/qcdhelper.go
+cp src/qcdscript.sh bin/qcdscript
